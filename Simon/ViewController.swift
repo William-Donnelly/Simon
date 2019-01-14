223276f9e57e453942a9f62f7bf1a47343ca1e11
William Donnelly <wdonnelly@Spaghett.local>
2019-01-14 13:11:15 -0600

    Initial Commit

---
+++ Simon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon
+//
+//  Created by William Donnelly on 1/14/19.
+//  Copyright © 2019 William Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
